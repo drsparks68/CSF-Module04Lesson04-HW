4b189f00420b9baa38166ee8c5d753f8395566a1
Derek Sparks <drsparks@gmail.com>
2020-04-29 10:54:51 -0700

    Add project files.

---
+++ CSF-Module04Lesson04-HW/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF_Module04Lesson04_HW
+{
+    public class Address
+    {
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public string Apartment { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int ZipPlusFour { get; set; }
+
+        public string FullAddress
+        {
+            get 
+            {
+                return $"{ HouseNumber } { StreetName } { City }, { State } { ZipCode }+{ ZipPlusFour } ";
+            }
+        }
+
+    }
+}
---
+++ CSF-Module04Lesson04-HW/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF_Module04Lesson04_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework: Create a class that has properties for street address, city state and zip code.  Then add a FullAddress property that is read-only.
+
+        Address myAddress = new Address();
+
+        myAddress.StreetName = "35th Ave S";
+        myAddress.HouseNumber = "7732";
+        myAddress.Apartment = "";
+        myAddress.City = "Anchorage";
+        myAddress.State = "AK";
+        myAddress.ZipCode = 12121;
+        myAddress.ZipPlusFour = 8888;
+
+            Console.WriteLine(myAddress.FullAddress);
+
+        Console.ReadLine();
+        }
+    }
+}
